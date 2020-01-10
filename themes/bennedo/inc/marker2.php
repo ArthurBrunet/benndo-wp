da3715c57d129cd9b5e665f8934ed32d27addd1e
ArthurBrunet <43806925+ArthurBrunet@users.noreply.github.com>
2020-01-10 16:50:12 +0100

    fix var geojson

--- themes/bennedo/inc/marker2.php
+++ themes/bennedo/inc/marker2.php
@@ -58,10 +58,17 @@ $api = json_decode('
     const json = '{\n  "0": [\n    { \n    "id":"id1",\n    "coords":"SRID=4326;POINT(1.52074078929 43.5615354615)",\n    "name":"pas renseign\u00e9",\n    "city":"Saint-111111111111111",\n    "city_code":31506,\n    "created_at":{"date":"2020-01-0911:05:37.000000","timezone_type":3,"timezone":"Europe\/Berlin"},\n    "updated_at":{"date":"2020-01-0914:20:42.000000","timezone_type":3,"timezone":"Europe\/Berlin"}\n    }\n  ],\n  "1": [\n    {\n    "id":"id2",\n    "coords":"SRID=4326;POINT(1.62074078929 43.6615354615)",\n    "name":"pas renseign\u00e9",\n    "city":"Saint-2222222222222222222",\n    "city_code":31506,\n    "created_at":{"date":"2020-01-0911:05:37.000000","timezone_type":3,"timezone":"Europe\/Berlin"},\n    "updated_at":{"date":"2020-01-0914:20:42.000000","timezone_type":3,"timezone":"Europe\/Berlin"}\n    }\n  ],\n    "2": [\n    {\n    "id":"id3",\n    "coords":"SRID=4326;POINT(1.65074078929 43.5615354615)",\n    "name":"pas renseign\u00e9",\n    "city":"Saint-33333333333333333333333",\n    "city_code":31506,\n    "created_at":{"date":"2020-01-0911:05:37.000000","timezone_type":3,"timezone":"Europe\/Berlin"},\n    "updated_at":{"date":"2020-01-0914:20:42.000000","timezone_type":3,"timezone":"Europe\/Berlin"}\n    }\n  ]\n  \n }';
     var objs = JSON.parse(json);
 
-    var result = [];
+    var geojson =
+        {
+            "type": "FeatureCollection",
+            "features": [
+
+            ]
+        };
+
     for(var index in objs) {
         var attr = objs[index];
-        result.push (
+        geojson.features.push (
             {
                 "type": "Feature",
                 "geometry": {
@@ -75,13 +82,7 @@ $api = json_decode('
                 }
             },
         )}
-
-    //console.log(result)
-
-    var a = "connard";
-
-    a += result;
-    console.log(a);
+console.log(geojson);
 </script>
 
 <style>
@@ -116,58 +117,11 @@ $api = json_decode('
     var a = 1;
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2FuYXJwcDIiLCJhIjoiY2szazZ6bnJjMDgwYzNtbm1zNHFocGZzNiJ9._V5QyjDorkoGktSpNHc1nA';
 
-    var geojson = {
-        "type": "FeatureCollection",
-        "features": [
-
-            document.write(result2);
-
-        ]
-    }
-
-    //console.log(geojson)
-
-    /*var geojson = {
-        "type": "FeatureCollection",
-        "features": [
-
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-77.032, 38.913]
-            },
-            "properties": {
-                "city": "C'est un titre",
-                "id": "bla12id",
-                "status" : 1,
-            }
-        },
-
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-122.414, 37.776]
-            },
-            "properties": {
-                "city": "C'est un titre",
-                "id": "bla12id",
-                "status" : 1,
-            }
-        }
-
-        ]
-    };*/
-
-
-
-
     // Create the map
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v9',
-        center: [-96, 37.8],
+        center: [-77, 37.8],
         zoom: 3
     });
 
@@ -183,7 +137,7 @@ $api = json_decode('
     geojson.features.forEach(function(marker) {
 
         // if the trash are not full neither broken
-        if (marker.properties.full === 0 && marker.properties.broken === 0) {
+        if (marker.properties.status === 0) {
 
             // create a HTML element for each feature
             var el = document.createElement('div');
@@ -205,28 +159,7 @@ $api = json_decode('
                 .addTo(map);
         }
 
-        // if the trash are full but not broken
-        if (marker.properties.full === 1 && marker.properties.broken === 0) {
 
-            // create a HTML element for each feature
-            var el = document.createElement('div');
-            el.className = 'marker_red';
-
-            // make a marker for each feature and add it to the map
-            new mapboxgl.Marker(el)
-                .setLngLat(marker.geometry.coordinates)
-                .setPopup(new mapboxgl.Popup({offset: 25}) // add popups
-                    .setHTML(
-                        '<h5>' + marker.properties.city + '</h5><br>' +
-                        '<h6>' + marker.properties.id + '</h6><br>' +
-                        '<button type="button" class="btn btn-info mr-3" onclick=""><?= $img_navigate ?></button>' +
-                        '<button type="button" class="btn btn-danger mr-3" onclick="trash_full()"><?= $img_trash_full ?></button>' +
-                        '<button type="button" class="btn btn-danger" onclick="broken_full()"><?= $img_trash_broken ?></button>'
-                    )
-                )
-
-                .addTo(map);
-        }
 
 
     });
